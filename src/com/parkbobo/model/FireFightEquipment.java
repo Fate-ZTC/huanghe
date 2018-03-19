e6d59ff08f6919fce80e5347177dfc4a744151b5
Nanxiyang <nan-official@outlook.com>
2018-03-19 14:09:17 +0800

    消防实体整合

---
+++ src/com/parkbobo/model/FireFightEquipment.java
@@ -0,0 +1,113 @@
+package com.parkbobo.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="fire_fight_equipment")
+@SequenceGenerator(name="generator", sequenceName="fire_fight_equipment_id_seq", allocationSize = 1)
+public class FireFightEquipment implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3369331990416418104L;
+	/**
+	 * id
+	 */
+	private Integer id;
+	/**
+	 * 设备状态 1正常 2异常
+	 */
+	private Short status;
+	/**
+	 * 设备名
+	 */
+	private String name;
+	/**
+	 * 经度
+	 */
+	private Float lon;
+	/**
+	 * 纬度
+	 */
+	private Float lat;
+	/**
+	 * 异常名称
+	 */
+	private Short checkStatus;
+	/**
+	 * 最后更新时间
+	 */
+	private Date lastUpdateTime;
+	/**
+	 * 校区
+	 */
+	private Integer campusNum;
+	@Id
+	@Column(name="id",nullable=false,unique=true)
+	@GeneratedValue(generator="generator", strategy = GenerationType.AUTO)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@Column(name="status")
+	public Short getStatus() {
+		return status;
+	}
+	public void setStatus(Short status) {
+		this.status = status;
+	}
+	@Column(name="name")
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Column(name="lon")
+	public Float getLon() {
+		return lon;
+	}
+	public void setLon(Float lon) {
+		this.lon = lon;
+	}
+	@Column(name="lat")
+	public Float getLat() {
+		return lat;
+	}
+	public void setLat(Float lat) {
+		this.lat = lat;
+	}
+	@Column(name="check_status")
+	public Short getCheckStatus() {
+		return checkStatus;
+	}
+	public void setCheckStatus(Short checkStatus) {
+		this.checkStatus = checkStatus;
+	}
+	@Column(name="last_update_time")
+	public Date getLastUpdateTime() {
+		return lastUpdateTime;
+	}
+	public void setLastUpdateTime(Date lastUpdateTime) {
+		this.lastUpdateTime = lastUpdateTime;
+	}
+	@Column(name="campus_num")
+	public Integer getCampusNum() {
+		return campusNum;
+	}
+	public void setCampusNum(Integer campusNum) {
+		this.campusNum = campusNum;
+	}
+}
---
+++ src/com/parkbobo/model/FirePatrolInfo.java
@@ -0,0 +1,129 @@
+package com.parkbobo.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="fire_patrol_info")
+@SequenceGenerator(name="generator", sequenceName="fire_patrol_info_id_seq", allocationSize = 1)
+public class FirePatrolInfo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7623324663655191769L;
+	/**
+	 * 异常信息id
+	 */
+	private Integer id;
+	/**
+	 * 用户区域
+	 */
+	private PatrolUser userId;
+	/**
+	 * 用户名
+	 */
+	private String userName;
+	/**
+	 * 校区
+	 */
+	private Integer campusNum;
+	/**
+	 * 时间戳
+	 */
+	private Date timestamp;
+	/**
+	 * 异常状态 1异常 2正常
+	 */
+	private Integer patrolStatus;
+	/**
+	 * 异常码
+	 */
+	private String excptionTypes;
+	/**
+	 * 设备id
+	 */
+	private Integer equipmentId;
+	/**
+	 * 异常描述
+	 */
+	private String description;
+	
+	@Id
+	@Column(name="id",nullable=false,unique=true)
+	@GeneratedValue(generator="generator", strategy = GenerationType.AUTO)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@OneToOne
+	@JoinColumn(name="user_id")
+	public PatrolUser getUserId() {
+		return userId;
+	}
+	public void setUserId(PatrolUser userId) {
+		this.userId = userId;
+	}
+	@Column(name="username")
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	@Column(name="campus_num")
+	public Integer getCampusNum() {
+		return campusNum;
+	}
+	public void setCampusNum(Integer campusNum) {
+		this.campusNum = campusNum;
+	}
+	@Column(name="timestamp")
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	@Column(name="patrol_status")
+	public Integer getPatrolStatus() {
+		return patrolStatus;
+	}
+	public void setPatrolStatus(Integer patrolStatus) {
+		this.patrolStatus = patrolStatus;
+	}
+	@Column(name="excption_types")
+	public String getExcptionTypes() {
+		return excptionTypes;
+	}
+	public void setExcptionTypes(String excptionTypes) {
+		this.excptionTypes = excptionTypes;
+	}
+	@Column(name="equipment_id")
+	public Integer getEquipmentId() {
+		return equipmentId;
+	}
+	public void setEquipmentId(Integer equipmentId) {
+		this.equipmentId = equipmentId;
+	}
+	@Column(name="description")
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
