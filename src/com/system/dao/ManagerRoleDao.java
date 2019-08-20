e5fba3f22eb6dc3c186db6dd9dc88811ae36a14c
zhangtiancheng <fate_ztc@163.com>
2019-08-20 18:46:36 +0800

    增加复选框

---
+++ src/com/system/dao/ManagerRoleDao.java
@@ -0,0 +1,8 @@
+package com.system.dao;
+
+import com.parkbobo.dao.BaseDao;
+import com.system.model.Manager;
+import com.system.model.ManagerRole;
+
+public interface ManagerRoleDao extends BaseDao<ManagerRole> {
+}
