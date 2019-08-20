6931ae37d4c7f69a9e4b74bc0a41ead089405faa
zhangtiancheng <fate_ztc@163.com>
2019-08-20 18:38:07 +0800

    增加复选框

---
+++ src/com/system/model/ManagerRoleId.java
@@ -0,0 +1,11 @@
+package com.system.model;
+
+/**
+ * @ClassName ManagerRoleId
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2019/8/20 18:37
+ * @Version 1.0
+ **/
+public class ManagerRoleId {
+}
