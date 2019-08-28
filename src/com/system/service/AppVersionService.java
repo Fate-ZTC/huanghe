04c2ac909492704b9c168c45fbfa75520e90469f
18030780015 <1127935472@qq.com>
2019-08-28 15:07:12 +0800

    修护模糊查询Bug

--- src/com/system/service/AppVersionService.java
+++ src/com/system/service/AppVersionService.java
@@ -8,28 +8,28 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 
 @Component("appVersionService")
-public class AppVersionService 
+public class AppVersionService
 {
 	@Resource(name="appVersionDaoImpl")
 	private AppVersionDao appVersionDao;
-	
+
 	public void save(AppVersion appVersion)
 	{
 		this.appVersionDao.merge(appVersion);
 	}
-	
+
 	public void delete(String versionCode)
 	{
 		AppVersion appVersion = this.appVersionDao.get(versionCode);
 		appVersion.setIsDel(1);
 		this.appVersionDao.merge(appVersion);
 	}
-	
+
 	public PageBean<AppVersion> getPage(String hql, int pageSize, int page)
 	{
 		return this.appVersionDao.pageQuery(hql, pageSize, page);
 	}
-	
+
 //	public List<AppVersion> getByHql(String hql)
 //	{
 //		return this.appVersionDao.getByHQL(hql);
@@ -39,7 +39,7 @@ public class AppVersionService
 
 		return this.appVersionDao.pageQuery(hql, pageSize, page);
 	}
-	
+
 	public AppVersion getById(String versionCode)
 	{
 		return this.appVersionDao.get(versionCode);
